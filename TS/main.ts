9b36ff686b943231d6660893cb277d97f1d7c23d
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-10-09 18:55:25 +0300

    Create function findAvailableClassrooms

--- TS/main.ts
+++ TS/main.ts
@@ -31,3 +31,28 @@ function addLesson(lesson: Lesson): boolean {
     schedule.push(lesson);
     return true; 
 }
+
+function findAvailableClassrooms (timeSlot: TimeSlot, dayOfWeek: DayOfWeek) : Array<string>{
+    const res: Array<string> = [];
+
+    for (const classroom of classrooms) {
+        let isAvailable = true;
+
+        for (const lesson of schedule) {
+            if (
+                lesson.classroomNumber === classroom.number &&
+                lesson.dayOfWeek === dayOfWeek &&
+                lesson.timeSlot === timeSlot
+            ) {
+                isAvailable = false; 
+                break;
+            }
+        }
+
+        if (isAvailable) {
+            res.push(classroom.number);
+        }
+    }
+
+    return res;
+}
