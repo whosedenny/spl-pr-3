227a27421fbd25714ddedce326317af048eef40e
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-10-09 17:52:37 +0300

    Create function addProfessor for adding profesot in array

--- TS/main.ts
+++ TS/main.ts
@@ -2,3 +2,7 @@ let professors: Array<Professor> = [];
 let classrooms: Array<Classroom> = [];
 let courses: Array<Course> = [];
 let schedule: Array<Lesson> = [];
+
+function addProfessor (professor: Professor): void {
+    professors.push(professor);
+}
