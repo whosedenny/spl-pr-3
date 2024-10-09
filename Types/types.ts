3298ac588a42c2b4b295b076111b8b390a92228c
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-10-09 15:53:55 +0300

    Create type aliase Professor

--- Types/types.ts
+++ Types/types.ts
@@ -2,4 +2,10 @@ type DayOfWeek = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday";
 
 type TimeSlot = "8:30-10:00" | "10:15-11:45" |  "12:15-13:45" | "14:00-15:30" | "15:45-17:15";
 
-type CourseType = "Lecture" | "Seminar" | "Lab" | "Practice"
+type CourseType = "Lecture" | "Seminar" | "Lab" | "Practice";
+
+type Professor = {
+    id : number,
+    name : string,
+    department: string
+}
