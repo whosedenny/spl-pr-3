eb548df2a2a4abd44c22394912ef51b47afd2615
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-10-09 15:51:58 +0300

    Create type aliase CourseType

--- Types/types.ts
+++ Types/types.ts
@@ -1,3 +1,5 @@
 type DayOfWeek = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday";
 
 type TimeSlot = "8:30-10:00" | "10:15-11:45" |  "12:15-13:45" | "14:00-15:30" | "15:45-17:15";
+
+type CourseType = "Lecture" | "Seminar" | "Lab" | "Practice"
