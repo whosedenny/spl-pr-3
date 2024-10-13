b7d0298eba74c85bab570a278cf60bf846b6eaf2
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-10-13 12:00:15 +0300

    Configuration eslint.config.mjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettier from 'eslint-plugin-prettier';
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default {
+  extends: [
+    pluginJs.configs.recommended,  
+    ...tseslint.configs.recommended 
+  ],
+  files: ["**/*.{js,mjs,cjs,ts}"],
+  languageOptions: {
+    ecmaVersion: 2020,  
+    globals: globals.browser,  
+  },
+  plugins: {
+    prettier, 
+  },
+  rules: {
+    'prettier/prettier': 'error',  
+  }
+};
